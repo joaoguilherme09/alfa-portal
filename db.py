e887f1509b6d557bc453e7d1e72995180b01fb66
joaoguilherme09 <joaoguipmendes@gmail.com>
2026-06-25 18:34:42 -0300

    add outro banco

--- db.py
+++ db.py
@@ -4,11 +4,11 @@ from mysql.connector import Error
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host="zephyr.proxy.rlwy.net",
-            user="root",
-            password="mesNlDsQeXxAYRfWdTOJOMvBvHRWixsE",
-            database="railway",
-            port=19496
+            host="auth-db1576.hstgr.io",
+            user="u726483483_alfa_user",
+            password="Alfa25062026profissionalizantes",
+            database="u726483483_alfa_portal",
+            port=3306
         )
         if connection.is_connected():
             print("MYSQL CONECTADO!")
